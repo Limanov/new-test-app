a7b66bc8c53f8540b864f8ece7cb99b735da5746
Limanov <kamilkalota@gmail.com>
2020-05-18 10:17:16 +0200

    feat: menu change

--- src/Components/MenuList.tsx
+++ src/Components/MenuList.tsx
@@ -1,37 +1,33 @@
-import React, { Component, Fragment, ComponentType} from "react";
+import React, { Component, Fragment, ComponentType } from "react";
 import logo from './../Img/logo.svg';
-import {LinkList} from './../Model/LinkList';
+import { LinkList } from './../Model/LinkList';
 import { multiValueLabelCSS } from "react-select/src/components/MultiValue";
 // import './leftMenu.css';
 type Props = {
-text:string;
+  text: string;
 };
-var testLinkArray: { name: string; label: string; }[] = [];
-for ( var index=0; index<LinkList.length; index++ ) {
-  // testLinkArray.push( LinkList[index] );
-  // testLinkArray.push( );
-  LinkList.map(e => {testLinkArray[index].name = e.label; testLinkArray[index].label = e.name});
-}
+var testLinkArray =
+  LinkList.map(e => { return { name: e.name, label: e.label } });
+
 // const testLinkArray = [label,name]
 // LinkList.map(e => {label:e.label,name:e.name})
-const MenuList: React.FC<Props> = ({text}: Props) => {
+const MenuList: React.FC<Props> = ({ text }: Props) => {
   return (
     <>
       <div className="leftMenu">
         <div className="flexMenu">
-            {/* {testLinkArray.map(element => <div>{element.name}</div>)} */}
-            {LinkList.map(element => <div>{element.label}</div>)}
-         <div className="image"><img src={logo}/></div>
-         <div className="text">{text}</div>
+          {testLinkArray.map(element => <div><a href={element.name}>{element.label}</a></div>)}
+          <div className="image"><img src={logo} /></div>
+          <div className="text">{text}</div>
         </div>
-          <div>  
-         <div className="content">
-             <hr/>
-             tekst<br/>
-             <hr/> 
-         </div>
-        <input />
+        <div>
+          <div className="content">
+            <hr />
+             tekst<br />
+            <hr />
           </div>
+          <input />
+        </div>
       </div>
     </>
   );
--- src/Model/LinkList.ts
+++ src/Model/LinkList.ts
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
 export const LinkList = [
-    {name:"index.html" , label:"Strona główna"},
-    {name:"/jakasstrona.tsx",label:"Jakaś strona"}
+  { name: "index.html", label: "Strona główna" },
+  { name: "jakasstrona.html", label: "Jakaś strona" },
 ];
